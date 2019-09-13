b3e49e6afa0605ee7dc2774a684cf6f874cb6408
fengchang <fengchang_fight@163.com>
2019-09-13 22:40:40 +0800

    cleaner file description

--- sconslearning/AnotherAdd.cpp
+++ sconslearning/AnotherAdd.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include "add.h"
+#include "minus.h"
 int main()
 {
     int a = 3;
     int b = 5;
     int c = add(a, b);
+    int d = minus(a, b);
     std::cout << "add result:" << c << "\n";
+    std::cout << "minus result:" << d << "\n";
+
     return 0;
 }
--- sconslearning/Hello.cpp
+++ sconslearning/Hello.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include "add.h"
+#include "minus.h"
 int main()
 {
-	int a=1;
-	int b=2;
-	int c=add(a,b);
-	std::cout<<"add result:"<<c<<"\n";
+	int a = 1;
+	int b = 2;
+	int c = add(a, b);
+	int d = minus(a, b);
+	std::cout << "add result:" << c << "\n";
+	std::cout << "minus result:" << d << "\n";
+
 	return 0;
 }
